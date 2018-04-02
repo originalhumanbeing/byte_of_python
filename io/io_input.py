ffb6b7f2480eb5403de73ace82b8ea0408560faf
originalhumanbeing <han.hyunjung17@gmail.com>
2018-04-03 00:44:31 +0900

    Modified io_input using regular expression, assert

--- io/io_input.py
+++ io/io_input.py
@@ -1,55 +1,43 @@
+import re
+
+
 def reverse(text):
     return text[::-1]
 
+
 def remove_punctuation(text):
-    if '.' in text:
-        result = text.replace(".", "")
-    if '?' in text:
-        result = text.replace("?", "")
-    if "!" in text:
-        result = text.replace("!", "")
-    if ':' in text:
-        result = text.replace(":", "")
-    if ';' in text:
-        result = text.replace(";", "")
-    if '-' in text:
-        result = text.replace("-", "")
-    if '()' in text:
-        result = text.replace("()", "")
-    if '[]' in text:
-        result = text.replace("[]", "")
-    if '...' in text:
-        result = text.replace("...", "")
-    if '\'' in text:
-        result = text.replace("\'", "")
-    if '""' in text:
-        result = text.replace('""', "")
-    if '\\' in text:
-        result = text.replace("\\", "")
-    if ',' in text:
-        result = text.replace(",", "")
-    else:
-        result = reverse(text)
-    return result
+    return re.sub(r'[?!.\"\'\[\]();:\-\\,]', '', text)
+
 
 def remove_space(text):
     return text.replace(" ", "")
 
+
 def low_case(text):
     return text.lower()
 
+
 def processing_text(text):
-    return low_case(remove_space(remove_punctuation(text)))
+    processed_text = low_case(remove_space(remove_punctuation(text)))
+    print(processed_text)
+    return processed_text
+
 
 def is_palindrome(text):
-    processed_text = processing_text(text)
-    return processed_text == low_case(remove_space(remove_punctuation(reverse(text))))
+    return processing_text(text) == processing_text(reverse(text))
 
-something = input('Enter text: ')
 
-if is_palindrome(something):
-    print('Yes, it is a palindrome')
+# something = input('Enter text: ')
 
-else:
-    print('No, it is not a palindrome')
+# if is_palindrome(something):
+#     print('Yes, it is a palindrome')
+#
+# else:
+#     print('No, it is not a palindrome')
+
+assert is_palindrome("Rise to Vote, Sir") is True
+assert is_palindrome("Ris..e to Vote, Sir") is True
+assert is_palindrome("A nut for a jar of tuna.") is True
+
+
 
